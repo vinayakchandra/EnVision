d8cf63fcac90da0e8c72b53618ecd146edae70ed
vinayak <vinayak.chandra.suryavanshi@gmail.com>
2025-12-06 03:42:53 +0530

    main controller to splashViewController

--- Envision/SceneDelegate.swift
+++ Envision/SceneDelegate.swift
@@ -18,8 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-//        window.rootViewController = SplashViewController()
-        window.rootViewController = MainTabBarController()
+       window.rootViewController = SplashViewController()
+        // window.rootViewController = MainTabBarController()
 //        window.rootViewController = RoomFurniture()
         self.window = window
         window.makeKeyAndVisible()
