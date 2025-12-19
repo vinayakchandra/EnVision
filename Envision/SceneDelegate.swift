d8e550d45872b7d948efd2e7839756a92c51eb81
vinayak <vinayak.chandra.suryavanshi@gmail.com>
2025-12-20 01:49:19 +0530

    only FIRST launch to be in dark mode

--- Envision/SceneDelegate.swift
+++ Envision/SceneDelegate.swift
@@ -26,7 +26,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let style: UIUserInterfaceStyle
 
         if let saved = UserDefaults.standard.object(forKey: "selectedTheme") as? Int {
-            style = saved == 0 ? .light : .dark
+            switch saved {
+            case 0:
+                style = .light
+            case 1:
+                style = .dark
+            default:
+                style = .unspecified // System
+            }
         } else {
             style = .unspecified // system mode on first launch
         }
